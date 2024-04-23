39287f2cfbfa63cde1532a5860f2dcb73bad6346
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2024-04-23 15:17:37 -0300

    feat: splitting routes

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,14 @@
 import fastify from "fastify"
-import { z } from "zod"
-import { PrismaClient } from "@prisma/client"
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify()
-const prisma = new PrismaClient({
-  log: ['query'],
-})
 
-// Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ... utilizar por questão de semântica
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-// Corpo da requisição (Request Body)
-// Parâmetros de busca (Search Params / Query Params) `http://localhost:3333/users?name=Isis`
-// Parâmetros de rota (Route Params) -> identificação de recursos `DELETE http://localhost:3333/users/3`
-// Cabeçalhos (Headers) -> Contexto
-
-// Semânticas = Significado
-
-// Driver nativo / Query Builder / ORMs
-
-// JSON - JavaScript Object Notation
-
-app.post('/events', async (request, reply) => { 
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable()
-  })
-  const data = createEventSchema.parse(request.body)
-
-  const event = await prisma.event.create({
-    data: {
-      title: data.title,
-      details: data.details,
-      maximumAttendees: data.maximumAttendees,
-      slug: new Date().toISOString(),
-    }
-  })
-
-  // return { eventId: event.id }
-  return reply.status(201).send({eventId: event.id})
-
-})
+app.register(createEvent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('http server running!')
